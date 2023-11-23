da70d700850f8d10c54d67cc0c68d82dab820007
jkuzmanovik <jordan.kuzmanovik@gmail.com>
2023-11-23 16:33:45 +0100

    ne e loso so control print f

--- components/RenderCV.tsx
+++ components/RenderCV.tsx
@@ -1,13 +1,9 @@
 "use client";
 import { useEffect, useState } from "react";
-import dynamic from "next/dynamic";
 import { Button } from "./ui/button";
-
-const Html2Pdf = dynamic(() => import("html2pdf.js"), { ssr: false });
 
 const RenderCV = () => {
   const [htmlContent, setHtmlContent] = useState("");
-  const [showButton, setShowButton] = useState(false);
 
   useEffect(() => {
     // Replace this with your API endpoint to fetch the HTML page
@@ -19,26 +15,23 @@ const RenderCV = () => {
       .catch((error) => console.error(error));
   }, []);
 
-  const downloadPdf = () => {
-    import('html2pdf.js')
-      .then(html2pdf => {
-        const element = document.createElement('div');
-        element.innerHTML = htmlContent;
-        html2pdf.default().from(element).save('downloaded-cv.pdf');
-      })
-      .catch(error => console.error('Failed to load html2pdf.js', error));
-  }
-
   useEffect(() => {
     // This is where you can do something with the HTML content
     console.log(htmlContent);
   }, [htmlContent]);
 
+  const printDocument = () => {
+    document.title = "My_CV";
+    window.print();
+  }
+  
+  
+
+
   return (
     <>
-          <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-          <br />
-    <Button onClick={downloadPdf}>Show button</Button>
+      <Button onClick={printDocument}>Print as PDF</Button>
+      <div dangerouslySetInnerHTML={{ __html: htmlContent }} id="cv-content"/>
     </>
   );
 };
